85a3d433f2259d82af09070edab80858a8f5bb3b
LuisMatheusVdeFarias <116664337+luismatheus2006@users.noreply.github.com>
2024-12-10 19:25:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int imprime_histograma(int* array,int i, int categorias);
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main(){
+    int categoria,i = 0;
+    int* vetor;
+    scanf("%d", &categoria);
+    vetor = malloc(categoria*sizeof(int));
     
-    return 0;
+    for(i; i < categoria; i++){
+        scanf("%d", &vetor[i]);
+    }
+    imprime_histograma(vetor, i , categoria);
+    free(vetor);
+}
+
+
+int imprime_histograma(int* array,int i, int categorias){
+    i = 0;
+    for(i; i < categorias; i++){
+        printf("%d ", array[i]);
+        for(; array[i] > 0; array[i]--){
+            printf("*");
+        }
+        printf("\n");
+    }
 }
